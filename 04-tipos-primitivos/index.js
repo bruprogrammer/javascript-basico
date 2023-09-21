b1f021976ece3568a878f7730fb4dd8efaf4afbb
bruna__duarte <106108275+bruprogrammer@users.noreply.github.com>
2023-09-21 10:48:45 -0300

    Add files via upload

---
+++ 04-tipos-primitivos/index.js
@@ -0,0 +1,22 @@
+//tipo primitivo string
+let nome = 'Bruna'
+
+//tipo primitivo number
+let numero = 1230
+let number = 5.0986
+
+//tipo primitivo boolean
+let usuarioVerificado = true
+
+    if(usuarioVerificado){
+        console.log("usuário verificado")
+    }else{
+        console.log("usuário não é verifivado")
+    }
+
+//tipo primitivo null (nulo/n existe/inválido)
+let pessoa = null
+
+//tipo primitivo undefined
+let telefone = undefined
+console.log(telefone)
